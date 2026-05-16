0464cd6cbf6889a86bdbbb46a431cec0010861d3
Ujwal Barodia <ujwalbarodia@gmail.com>
2026-05-16 18:03:36 +0530

    env changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://collaborative-code-editor-server.onrender.com/',
-  websocketUrl: 'wss://collaborative-code-editor-server.onrender.com/',
+  apiUrl: 'https://collaborative-code-editor-server-r7uc.onrender.com/',
+  websocketUrl: 'wss://collaborative-code-editor-server-r7uc.onrender.com/',
   appName: 'Collaborative Code Editor'
 };
